42b9320ce3c405885e11dcef8aec31a8e7256b59
Daniel <timertimer5@gmail.com>
2017-11-24 19:44:21 +0100

    Potential crash fix for NewHeistsGui (?)
    Removed the delayedcall but eh, need feedback on this change

--- lib/NewHeistsGui.lua
+++ lib/NewHeistsGui.lua
@@ -1,17 +1,15 @@
 Hooks:PostHook( NewHeistsGui, "init", "nepmenu_post_init_newheists", function(self, ws, fullscreen_ws)
-	DelayedCalls:Add( "AntiCrash_MovingNewHeists", 0.06, function()
-		local PANEL_PADDING = 10
+	local PANEL_PADDING = 10
 
-		if managers.menu_component._blt_notifications then
-			local header_h = tweak_data.menu.pd2_small_font_size + PANEL_PADDING
-			local wx = self._content_panel:world_x() - 25
-			local wy = managers.menu_component._blt_notifications._panel:world_y()
-			wx, wy = managers.gui_data:convert_pos(ws, fullscreen_ws, wx, wy)
+	if managers.menu_component._blt_notifications then
+		local header_h = tweak_data.menu.pd2_small_font_size + PANEL_PADDING
+		local wx = self._content_panel:world_x() - 25
+		local wy = managers.menu_component._blt_notifications._panel:world_y()
+		wx, wy = managers.gui_data:convert_pos(ws, fullscreen_ws, wx, wy)
 
-			self._content_panel:set_world_y(wy - header_h)
-			self._content_panel:set_x(wx)
-		else
-			self._content_panel:set_bottom(self._panel:height() - PANEL_PADDING * 2)
-		end
-	end)
+		self._content_panel:set_world_y(wy - header_h)
+		self._content_panel:set_x(wx)
+	else
+		self._content_panel:set_bottom(self._panel:height() - PANEL_PADDING * 2)
+	end
 end)
